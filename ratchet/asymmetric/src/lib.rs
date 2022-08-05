f72a3d8ad523c0eb82695682c81baac0d1559cd2
noot <elizabethjbinks@gmail.com>
2022-08-04 23:45:18 -0400

    address comments

--- ratchet/asymmetric/src/lib.rs
+++ ratchet/asymmetric/src/lib.rs
@@ -1,9 +1,11 @@
-pub mod asymmetric_ratchet;
+#[cfg(feature = "diffie_helman")]
+pub mod diffie_helman;
 
 pub trait AsymmetricRatchet {
   type PublicKey;
+  type Output;
 
   fn step<R: rand::CryptoRng + rand::RngCore>(&mut self, rng: R) -> Self::PublicKey;
 
-  fn handshake(&self, key: Self::PublicKey) -> Self::PublicKey;
+  fn handshake(&self, key: Self::PublicKey) -> Self::Output;
 }
