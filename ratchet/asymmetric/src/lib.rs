c70133226f2ab803f52ddf0b378d3c24de4c1ea0
noot <elizabethjbinks@gmail.com>
2022-08-04 23:47:51 -0400

    add AsRef back for Output

--- ratchet/asymmetric/src/lib.rs
+++ ratchet/asymmetric/src/lib.rs
@@ -3,8 +3,8 @@ pub mod diffie_helman;
 
 pub trait AsymmetricRatchet {
   type PublicKey;
-  type Output;
-
+  type Output: AsRef<[u8]>;
+  
   fn step<R: rand::CryptoRng + rand::RngCore>(&mut self, rng: R) -> Self::PublicKey;
 
   fn handshake(&self, key: Self::PublicKey) -> Self::Output;
